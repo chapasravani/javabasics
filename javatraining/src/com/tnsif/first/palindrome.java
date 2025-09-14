57197a8e0c01435fe6d56fd2b60fb07610113839
srava <srava@Sravanichapa>
2025-09-14 12:04:21 +0530

    palindrome

---
+++ javatraining/src/com/tnsif/first/palindrome.java
@@ -0,0 +1,44 @@
+package com.tnsif.first;
+
+import java.util.*;
+
+public class palindrome {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int original = num;
+        int[] digits = new int[20]; 
+        int count = 0;
+        while (num > 0) {
+            digits[count] = num % 10; 
+            count++;
+            num = num / 10;
+        }
+
+        
+        boolean isPalindrome = true;
+        for (int i = 0; i < count / 2; i++) {
+            if (digits[i] != digits[count - 1 - i]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Step 3: result
+        if (isPalindrome) {
+            System.out.println(original + " is a Palindrome.");
+        } else {
+            System.out.println(original + " is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+
+
+
+	}
+
+
