0f079b77e65581bbe7e835f2202baf78dab0077a
srava <srava@Sravanichapa>
2025-09-16 19:52:22 +0530

    inherited

---
+++ javatraining/src/com/tnsif/singleinheritance/classA.java
@@ -0,0 +1,19 @@
+package com.tnsif.singleinheritance;
+
+public class classA {
+	int a =10;
+	public void show()
+	{
+		System.out.println("This is Base class");
+	}
+
+}
+
+class B extends classA{
+	int b=20;
+	public void display()
+	{
+		System.out.println("This is Derived Class");
+	}
+	
+}
---
+++ javatraining/src/com/tnsif/singleinheritance/singleinheritance.java
@@ -0,0 +1,13 @@
+package com.tnsif.singleinheritance;
+
+public class singleinheritance {
+
+	public static void main(String[] args) {
+	 
+		B b=new B();
+		b.show();
+		b.display();
+
+	}
+
+}
