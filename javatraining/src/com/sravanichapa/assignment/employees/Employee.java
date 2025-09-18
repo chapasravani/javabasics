643af87bdf74f26876ba9693b463765c2f8bac95
srava <srava@Sravanichapa>
2025-09-18 22:23:08 +0530

    Assignment

---
+++ javatraining/src/com/sravanichapa/assignment/employees/Employee.java
@@ -0,0 +1,5 @@
+package com.sravanichapa.assignment.employees;
+
+public class Employee {
+
+}
---
+++ javatraining/src/com/sravanichapa/assignment/utilities/EmployeeUtilities.java
@@ -0,0 +1,5 @@
+package com.sravanichapa.assignment.utilities;
+
+public class EmployeeUtilities {
+
+}
